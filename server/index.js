61f900873c5ad397b6d7f3bea5082240c9e412a0
Evan Katz <evankatz14@gmail.com>
2021-07-26 00:13:20 -0700

    server setup

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const db = require('./postgres');
 const path = require('path');
 const router = require('./router');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const port = 3001;
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use('/', router);
 
